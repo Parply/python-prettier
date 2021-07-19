b6eafcf5ad54961a3229415d849beb71028e9aed
AlexPinches <alex.pinches@cqsm.com>
2021-07-19 18:36:52 +0100

    a

--- style_check.sh
+++ style_check.sh
@@ -22,31 +22,28 @@ function format {
             git commit -am 'Formatting'
             git push github HEAD:$GITHUB_HEAD_REF
         fi
-        echo "$COMMENT" 2>&1
-        if [[ "$COMMENT" -eq "true" ]]; then
-            exit 1
-            mypy $FILES --ignore-missing-imports --strict --install-types --non-interactive --pretty --python-version 3.7 > mypy_report.txt
-            pylint $FILES  --rcfile=./pylintrc > pylint_report.txt
-            COMMENT_MYPY=$(cat mypy_report.txt)
-            COMMENT_PYLINT=$(cat pylint_report.txt)
 
-            COMMENT_MAGIC_HEADER="Formatter"
-            MESSAGE="<summary> <b> PEP8 Standard Report </b></summary> \n $COMMENT_PYLINT \n <summary> <b> Typing Report </b></summary> \n $COMMENT_MYPY"
+        mypy $FILES --ignore-missing-imports --strict --install-types --non-interactive --pretty --python-version 3.7 > mypy_report.txt
+        pylint $FILES  --rcfile=./pylintrc > pylint_report.txt
+        COMMENT_MYPY=$(cat mypy_report.txt)
+        COMMENT_PYLINT=$(cat pylint_report.txt)
 
-            MESSAGE_BODY="$(jq -n \
-                --arg COMMENT_MAGIC_HEADER "$COMMENT_MAGIC_HEADER" \
-                --arg MESSAGE "$MESSAGE" \
-                '{ body: ($COMMENT_MAGIC_HEADER + "\n" + ($MESSAGE | sub( "^[\\s\\p{Cc}]+"; "" ) | sub( "[\\s\\p{Cc}]+$"; "" ))) }' \
-            )"
+        COMMENT_MAGIC_HEADER="Formatter"
+        MESSAGE="<summary> <b> PEP8 Standard Report </b></summary> \n $COMMENT_PYLINT \n <summary> <b> Typing Report </b></summary> \n $COMMENT_MYPY"
 
-            PR_NUMBER=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
+        MESSAGE_BODY="$(jq -n \
+            --arg COMMENT_MAGIC_HEADER "$COMMENT_MAGIC_HEADER" \
+            --arg MESSAGE "$MESSAGE" \
+            '{ body: ($COMMENT_MAGIC_HEADER + "\n" + ($MESSAGE | sub( "^[\\s\\p{Cc}]+"; "" ) | sub( "[\\s\\p{Cc}]+$"; "" ))) }' \
+        )"
 
-            COMMENT_HTML_URL="$(auth_crl "$GITHUB_API/issues/$PR_NUMBER/comments" \
-                -X POST \
-                -H "Content-Type: application/json" \
-                --data "$MESSAGE_BODY" \
-            | jq -r '.html_url' )"
-        fi
+        PR_NUMBER=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
+
+        COMMENT_HTML_URL="$(auth_crl "$GITHUB_API/issues/$PR_NUMBER/comments" \
+            -X POST \
+            -H "Content-Type: application/json" \
+            --data "$MESSAGE_BODY" \
+        | jq -r '.html_url' )"
     fi
 }
 
