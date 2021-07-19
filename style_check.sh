e7f68a8ecd7cb41039e856fb7238016ee715aeb4
AlexPinches <alex.pinches@cqsm.com>
2021-07-19 17:40:53 +0100

    a

--- style_check.sh
+++ style_check.sh
@@ -18,7 +18,7 @@ function format {
             pylint $FILES  --rcfile=./pylintrc > pylint_report.txt
             COMMENT_MYPY=$(cat mypy_report.txt)
             COMMENT_PYLINT=$(cat pylint_report.txt)
-            curl -X POST $URL -H "Content-Type: application/json" -H "Authorization: token $GITHUB_TOKEN" --data "{ 'body': '<summary> <b> PEP8 Standard Report </b></summary> \n $COMMENT_PYLINT \n <summary> <b> Typing Report </b></summary> \n $COMMENT_MYPY' }"
+            curl -X POST $URL -H "Content-Type: application/json" -H "Authorization: token $GITHUB_TOKEN" --data "{ \'body\': \'<summary> <b> PEP8 Standard Report </b></summary> \n $COMMENT_PYLINT \n <summary> <b> Typing Report </b></summary> \n $COMMENT_MYPY\' }"
         fi
     fi
 }
