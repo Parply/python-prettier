60abeefb07fdb4ca7d268fdb39482ec078687ec5
AlexPinches <alex.pinches@cqsm.com>
2021-07-19 13:03:49 +0100

    test

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM python:3.7-buster
+
+ADD requirements.txt /requirements.txt
+ADD style_check.sh /style_check.sh
+RUN sudo apt-get install -y enchant
+RUN pip install -r requirements.txt
+
+ENTRYPOINT ["style_check.sh"]
---
+++ style_check.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+function get_files {
+    git diff --name-only HEAD^..HEAD | grep '*.py' | echo
+}
+
+function format {
+    FILES = get_files
+    isort $FILES
+    black $FILES
+    if ! git diff-files --quiet; then
+        git commit -am 'Formatting'
+        git push
+    fi
+
+    if $COMMENT; then
+        mypy $FILES --ignore-missing-imports --strict --install-types --non-interactive --pretty --python-version 3.7 > mypy_report.txt
+        pylint $FILES  --rcfile=./pylintrc > pylint_report.txt
+        COMMENT_MYPY = $(cat mypy_report.txt)
+        COMMENT_PYLINT = $(cat pylint_report.txt)
+        curl -X POST $URL -H "Content-Type: application/json" -H "Authorization: token $GITHUB_TOKEN" --data "{ 'body': '<summary> <b> PEP8 Standard Report </b></summary> \n $COMMENT_PYLINT \n <summary> <b> Typing Report </b></summary> \n $COMMENT_MYPY' }"
+    fi
+}
+
+### RUN ###
+
+format
