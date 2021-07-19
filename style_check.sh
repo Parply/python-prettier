0331cff7d40e52753f9927885d91135e9e569a33
AlexPinches <alex.pinches@cqsm.com>
2021-07-19 18:28:03 +0100

    a

--- style_check.sh
+++ style_check.sh
@@ -23,7 +23,7 @@ function format {
             git push github HEAD:$GITHUB_HEAD_REF
         fi
         echo "$COMMENT"
-        if [ "$COMMENT" = "true" ]; then
+        if [ "$COMMENT" -eq "true" ]; then
             exit 1
             mypy $FILES --ignore-missing-imports --strict --install-types --non-interactive --pretty --python-version 3.7 > mypy_report.txt
             pylint $FILES  --rcfile=./pylintrc > pylint_report.txt
