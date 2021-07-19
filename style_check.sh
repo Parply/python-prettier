5133fd7145cb42aed9dc4b195ddf3a8c5b45a120
AlexPinches <alex.pinches@cqsm.com>
2021-07-19 18:19:48 +0100

    a

--- style_check.sh
+++ style_check.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+function crl {
+curl --silent --show-error --location --retry 1 "${@:2}" \
+    -H "Accept: application/vnd.github.antiope-preview+json, application/vnd.github.v3+json" \
+    "$1"
+}
+
+function auth_crl {
+crl "$1" -H "authorization: Bearer $GITHUB_TOKEN" "${@:2}"
+}
+
 function format {
     FILES=$(git diff --name-only HEAD^..HEAD | grep \.py$)
     if [[ ! -z "$FILES" ]]; then 
@@ -18,6 +28,25 @@ function format {
             pylint $FILES  --rcfile=./pylintrc > pylint_report.txt
             COMMENT_MYPY=$(cat mypy_report.txt)
             COMMENT_PYLINT=$(cat pylint_report.txt)
+
+            COMMENT_MAGIC_HEADER="Formatter"
+            MESSAGE="<summary> <b> PEP8 Standard Report </b></summary> \n $COMMENT_PYLINT \n <summary> <b> Typing Report </b></summary> \n $COMMENT_MYPY"
+
+            MESSAGE_BODY="$(jq -n \
+                --arg COMMENT_MAGIC_HEADER "$COMMENT_MAGIC_HEADER" \
+                --arg MESSAGE "$MESSAGE" \
+                '{ body: ($COMMENT_MAGIC_HEADER + "\n" + ($MESSAGE | sub( "^[\\s\\p{Cc}]+"; "" ) | sub( "[\\s\\p{Cc}]+$"; "" ))) }' \
+            )"
+
+            PR_NUMBER=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
+
+            COMMENT_HTML_URL="$(auth_crl "$GITHUB_API/issues/$PR_NUMBER/comments" \
+                -X POST \
+                -H "Content-Type: application/json" \
+                --data "$MESSAGE_BODY" \
+            | jq -r '.html_url' )"
+
+
             curl -X POST -H "Content-Type: application/json" -H "Authorization: token $GITHUB_TOKEN" --data "{\'body\':\'<summary> <b> PEP8 Standard Report </b></summary> \n $COMMENT_PYLINT \n <summary> <b> Typing Report </b></summary> \n $COMMENT_MYPY\'}" $URL 
         fi
     fi
