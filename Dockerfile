8366bdaea5d594e992b3995c06b86db102b15d10
AlexPinches <alex.pinches@cqsm.com>
2021-07-20 11:48:18 +0100

    a

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,6 @@ FROM python:3.7-buster
 ADD requirements.txt /requirements.txt
 ADD style_check.sh /style_check.sh
 ADD .pylintrc /.pylintrc
-RUN apt-get update && apt-get install -y hunspell-en-gb
 RUN pip install -r requirements.txt
 
 ENTRYPOINT ["/bin/bash","./style_check.sh"]
--- style_check.sh
+++ style_check.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-function crl {
-curl --silent --show-error --location --retry 1 "${@:2}" \
-    -H "Accept: application/vnd.github.antiope-preview+json, application/vnd.github.v3+json" \
-    "$1"
-}
-
-function auth_crl {
-crl "$1" -H "authorization: Bearer $GITHUB_TOKEN" "${@:2}"
-}
-
 function format {
     FILES=$(git diff --name-only HEAD^..HEAD | grep \.py$)
     if [[ ! -z "$FILES" ]]; then 
@@ -24,15 +14,15 @@ function format {
         fi
 
         COMMENT_MYPY=$(mypy $FILES --ignore-missing-imports --strict --install-types --non-interactive --pretty --python-version 3.7)
-        COMMENT_PYLINT=$(pylint $FILES  --rcfile=./.pylintrc  --enable spelling --spelling-dict en_GB)
+        COMMENT_PYLINT=$(pylint $FILES  --rcfile=./.pylintrc)
 
         COMMENT_MAGIC_HEADER="Formatter"
         MESSAGE="<summary> <b> PEP8 Standard Report </b></summary> \n $COMMENT_PYLINT \n <summary> <b> Typing Report </b></summary> \n $COMMENT_MYPY"
         
-        curl -s -H "Authorization: token ${GITHUB_TOKEN}" \
+        curl -s --retry 1 -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+        -H "Accept: application/vnd.github.antiope-preview+json, application/vnd.github.v3+json" \
         -X POST -d '{ "body": "$MESSAGE", "start_line": 1, "start_side":"left"}' \
-        "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/issues/${PR_NUMBER}/comments"
-            
+        "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/issues/${PR_NUMBER}/comments"      
     fi
 }
 
