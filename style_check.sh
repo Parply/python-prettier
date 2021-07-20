eba8f6d08ac1253ab0405d5b665e9fe566cea160
AlexPinches <alex.pinches@cqsm.com>
2021-07-20 11:53:05 +0100

    a

--- style_check.sh
+++ style_check.sh
@@ -19,9 +19,7 @@ function format {
         COMMENT_MAGIC_HEADER="Formatter"
         MESSAGE="<summary> <b> PEP8 Standard Report </b></summary> \n $COMMENT_PYLINT \n <summary> <b> Typing Report </b></summary> \n $COMMENT_MYPY"
         
-        curl -s --retry 1 -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-        -H "Accept: application/vnd.github.antiope-preview+json, application/vnd.github.v3+json" \
-        -X POST -d '{ "body": "$MESSAGE", "start_line": 1, "start_side":"left"}' \
+        curl --retry 1 -H "Authorization: Token $GITHUB_TOKEN" -X POST -d '{ "body": "$MESSAGE", "start_line": 1, "start_side":"left"}' \
         "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/issues/${PR_NUMBER}/comments"      
     fi
 }
