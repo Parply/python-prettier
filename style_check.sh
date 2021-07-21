1d9ca2aaeebf4db005e51fdb95c701dbc12a2bdc
AlexPinches <alex.pinches@cqsm.com>
2021-07-21 12:10:02 +0100

    a

--- style_check.sh
+++ style_check.sh
@@ -12,7 +12,7 @@ function format {
             git commit -am 'Formatting'
             git push --force github HEAD:$GITHUB_HEAD_REF
         fi
-        mypy $FILES --ignore-missing-imports --strict --install-types --non-interactive --pretty --python-version 3.7 --junit-xml 'mypy.xml'
+        mypy $FILES --ignore-missing-imports --strict --install-types --non-interactive --pretty --python-version 3.7 --junit-xml mypy.xml
         cat mypy.xml | echo
         COMMENT_MYPY=$(grep -oP '(?<=failure message\=\"mypy produced messages\">)[^<]+' mypy.xml)
         COMMENT_PYLINT=$(pylint $FILES --enable spelling --spelling-dict en_GB --rcfile=./.pylintrc)
@@ -26,7 +26,7 @@ function format {
         echo $MESSAGE
         echo $GH_TOKEN
 
-        curl -s -S -u "$GITHUB_TOKEN:x-oauth-basic" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL"  #-H "Authorization: token $GITHUB_TOKEN"
+        curl -s -S -u "$GH_TOKEN:x-oauth-basic" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL"  #-H "Authorization: token $GITHUB_TOKEN"
     fi
 }
 
