79f6d68baa4cbdd85eacde7b7b190062492253c4
AlexPinches <alex.pinches@cqsm.com>
2021-08-02 18:44:37 +0100

    a

---
+++ comment_pr.py
@@ -0,0 +1,17 @@
+import os
+
+from github import Github
+
+if __name__ == '__main__':
+    GITHUB_TOKEN = os.getenv("GH_TOKEN")
+    GITHUB_REPOSITORY = os.getenv("GITHUB_REPOSITORY")
+    PR_NUMBER = os.getenv("PR_NUMBER")
+
+    MESSAGE = os.getenv("MESSAGE")
+
+    git = Github(GITHUB_TOKEN)
+
+    repo = git.get_repo(GITHUB_REPOSITORY)
+    pr = repo.get_pull(PR_NUMBER)
+
+    pr.create_issue_comment(MESSAGE)
