048e4394f40824154145d6013b0503723affd90d
AlexPinches <alex.pinches@cqsm.com>
2021-08-09 18:08:44 +0100

    a

--- comment_pr.py
+++ comment_pr.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     GITHUB_REPOSITORY = os.getenv("GITHUB_REPOSITORY")
     PR_NUMBER = os.getenv("PR_NUMBER")
 
-    MESSAGE = os.getenv("MESSAGE") + parse(".\mypy.xml").findall("./testcase/failure")[0].text
+    MESSAGE = os.getenv("MESSAGE") + parse("mypy.xml").findall("./testcase/failure")[0].text
 
     print(GITHUB_TOKEN,GITHUB_REPOSITORY,PR_NUMBER,MESSAGE)
 
