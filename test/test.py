4c235e85d23fb51edf028970ee45e2b89a550d9a
AlexPinches <alex.pinches@cqsm.com>
2021-08-09 18:58:12 +0100

    a

--- test/test.py
+++ test/test.py
@@ -6,8 +6,8 @@ import pandas as pd
 import functools
 
 
-def test(a):  
-    return 2*a + 1 + 4+ 3+ 6 +5 + 5 + 6+ 3 + 6+ 6+ 3+ 3+ 3+3 +3+3 *3+ 3 + 5 
+def test(a):     
+    return 2*a + 1 + 4+ 3+ 6 +5 + 5 + 6+ 3 +    6+ 6+ 3+ 3+ 3+3 +3+3 *3+ 3 + 5 
 
 if __name__ == '__main__':
     print('hello')
