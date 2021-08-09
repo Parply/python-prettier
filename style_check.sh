b779313f3458381babfddf1b645cc82ea343cae6
AlexPinches <alex.pinches@cqsm.com>
2021-08-09 17:43:04 +0100

    a

--- style_check.sh
+++ style_check.sh
@@ -23,7 +23,7 @@ function format {
         echo $MESSAGE
         echo $GH_TOKEN
         
-        python "comment_pr.py"
+        python ./comment_pr.py
 
         #curl -i -X POST -H "Authorization: token $GH_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL"  #-H "Authorization: token $GITHUB_TOKEN"
     fi
