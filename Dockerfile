35e80ef6a4e699b3da5da5ec78fc12aa7bcc3d56
AlexPinches <alex.pinches@cqsm.com>
2021-08-16 12:38:38 +0100

    a

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM python:3.7-buster AS FORMAT
 ADD requirements.txt /requirements.txt
 ADD main.sh /main.sh
-ADD comment_pr.py /comment_pr.py
-ADD .pylintrc /.pylintrc
+ADD comment_pr.py /github/workspace/comment_pr.py
+ADD .pylintrc /github/workspace/.pylintrc
 RUN curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
 RUN apt update -y && apt install -y jq hunspell-en-gb enchant nodejs
 RUN npm install --global poor-mans-t-sql-formatter-cli
--- comment_pr.py
+++ comment_pr.py
@@ -18,9 +18,9 @@ if __name__ == "__main__":
     MESSAGE = (
         HEADER
         + PEP_HEADER
-        + open("/pylint.txt", "r").read()
+        + open(f"{GITHUB_WORKSPACE}/pylint.txt", "r").read()
         + MYPY_HEADER
-        + parse("mypy.xml").findall("./testcase/failure")[0].text
+        + parse(f"{GITHUB_WORKSPACE}/mypy.xml").findall("./testcase/failure")[0].text
     )
 
     print(GITHUB_TOKEN, GITHUB_REPOSITORY, PR_NUMBER, MESSAGE)
