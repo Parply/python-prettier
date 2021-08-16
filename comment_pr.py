35cf4774341495137c6152fbc6f476ce81b9ef50
AlexPinches <alex.pinches@cqsm.com>
2021-08-16 16:43:43 +0100

    a

--- comment_pr.py
+++ comment_pr.py
@@ -1,29 +1,24 @@
 import os
+from html import escape
 
 from defusedxml.ElementTree import parse
 from github import Github
 
-HEADER = "<h3>:snake: Python Styling :snake:</h3>\n"
-PEP_HEADER = "<summary><b>PEP8 Standard Report (pylint)</b></summary>\n"
-MYPY_HEADER = "<summary><b>Typing Report (mypy)</b></summary>\n"
+HEADER = "<h2>:snake: Python Styling :snake:</h2><br>"
+PEP_HEADER = "<summary><h4><b>PEP8 Standard Report (pylint)</b></h4></summary><br>"
+MYPY_HEADER = "<summary><h4><b>Typing Report (mypy)</b></summary></h4><br>"
 if __name__ == "__main__":
     GITHUB_TOKEN = os.getenv("INPUT_GH_TOKEN")
     GITHUB_REPOSITORY = os.getenv("GITHUB_REPOSITORY")
     PR_NUMBER = int(os.getenv("INPUT_PR_NUMBER"))
-    GITHUB_WORKSPACE = os.getenv("GITHUB_WORKSPACE")
-
-    print(os.getcwd())
-    print(os.listdir())
 
     MESSAGE = (
         HEADER
         + PEP_HEADER
-        + open("/action/pylint.txt", "r").read().replace('\n',"<br>")
+        + escape(open("/action/pylint.txt", "r").read()).replace('\n',"<br>").replace(' ',"&nbsp;")
         + MYPY_HEADER
-        + parse("/action/mypy.xml").findall("./testcase/failure")[0].text.replace('\n',"<br>")
+        + escape(parse("/action/mypy.xml").findall("./testcase/failure")[0].text).replace('\n',"<br>").replace(' ',"&nbsp;")
     )
-
-    print(GITHUB_TOKEN, GITHUB_REPOSITORY, PR_NUMBER, MESSAGE)
 
     git = Github(GITHUB_TOKEN)
 
