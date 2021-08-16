1833b393e2896cda5a0d0754a8507f650d848df8
AlexPinches <alex.pinches@cqsm.com>
2021-08-16 18:35:35 +0100

    a

--- test/test2.py
+++ test/test2.py
@@ -1,4 +1,8 @@
 import torch
 
+def test(a:torch.Tensor):
+    return a +1 
+
 a = torch.ones((50, 50))
-out_put = torch.chain_matmul(a, a, a, a)
+out_put = torch.chain_matmul(a, a, a, a) + test(a)
+
